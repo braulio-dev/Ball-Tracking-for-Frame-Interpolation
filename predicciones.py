f37691b533ab01badb4c6746c046b46748c66887
RenataSz4 <renatasan0411@gmail.com>
2023-11-25 15:50:28 -0800

    metodos

--- predicciones.py
+++ predicciones.py
@@ -26,12 +26,14 @@ def VelocidadAngulo(x1, y1, x2, y2, cuadros):
     v = PixelesAMetros(v) * FPS
     return angulo, v
 
-
 def Punto(angulo, vi, distancia_x):
+    return Taylor(angulo, vi, distancia_x)
+
+def Taylor(angulo, vi, distancia_x):
     distancia_x = PixelesAMetros(distancia_x)
     cant_terminos = 5
     x = Symbol('x')
-    f = tan(angulo * pi / 180) * x + -9.81 * (x ** 2) / (2 * cos(angulo * pi / 180) * (vi ** 2))
+    f = tan(angulo * pi / 180) * x + -9.81 * (x ** 2) / (2 * (cos(angulo * pi / 180)**2) * (vi ** 2))
     xi = distancia_x - 1
     xi1 = distancia_x
     h = xi1 - xi
@@ -62,3 +64,63 @@ def Punto(angulo, vi, distancia_x):
 
     # Resultados
     return int(MetrosAPixeles(distancia_x)), int(MetrosAPixeles(st[-1]))  # (x, y)
+
+def Euler(angulo, vi, x0): # Metodo de euler
+    n = 10
+    x0 = PixelesAMetros(x0)
+    x0 = x0
+    x1 = x0 + 1
+    h = (x1 - x0)/n
+    x_values = np.arange(x0, x1 + h, h)
+    y_values = [0] * n
+
+    #Iterar desde el primer elemento hasta el penultimo
+    for x in x_values[:-1]:
+        # Calcular el cambio de la altura con respecto a la posición
+        dy_dx = np.tan(angulo * np.pi / 180) * x - (9.81 * x ** 2) / (2 * np.cos(angulo * np.pi / 180) * vi ** 2)
+
+        # Usar el método de Euler para obtener los valores de y
+        y_new = y_values[-1] + h * dy_dx
+        y_values.append(y_new)
+
+    return int(MetrosAPixeles(x0)), int(MetrosAPixeles(y_values[-1]))
+
+def RungeKutta(angulo, vi, x0):
+    n = 50
+    x0 = PixelesAMetros(x0)
+    x1 = x0 + 1
+    h = (x1 - x0) / n
+    x_values = np.arange(x0, x1 + h, h)
+    y_values = [0] * n
+
+    # Función de la trayectoria
+    def f(x, y):
+        return np.tan(angulo * np.pi / 180) * x - (9.81 * x ** 2) / (2 * np.cos(angulo * np.pi / 180) * vi ** 2)
+
+    # Método de Runge-Kutta
+    for x in x_values[:-1]:
+
+        k1 = h * f(x, y_values[-1])
+        k2 = h * f(x + h/2, y_values[-1] + k1/2)
+        k3 = h * f(x + h/2, y_values[-1] + k2/2)
+        k4 = h * f(x + h, y_values[-1] + k3)
+
+        # Calcular la nueva altura utilizando los coeficientes de Runge-Kutta
+        y_new = y_values[-1] + (k1 + 2*k2 + 2*k3 + k4) / 6
+        y_values.append(y_new)
+
+    return int(MetrosAPixeles(x0)), int(MetrosAPixeles(y_values[-1]))
+
+""" def InterpolacionLineal(angulo, vi, x0):
+    x0 = PixelesAMetros(x0)
+    x1 = x0 + 1
+
+    # Función de la trayectoria
+    def f(x):
+        return np.tan(angulo * np.pi / 180) * x - (9.81 * x ** 2) / (2 * np.cos(angulo * np.pi / 180) * vi ** 2)
+
+    # Calcular la posición y mediante interpolación lineal
+    fx = f(x0) + (((f(x1) - f(x0)) / (x1 - x0) * (x - x0)))
+
+    return int(MetrosAPixeles(x_interpolacion)), int(MetrosAPixeles(fx))
+"""
